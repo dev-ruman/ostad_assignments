ca8a18f3a71dbc20228a1853c5ceb2534ec6a794
dev-ruman <90115551+dev-ruman@users.noreply.github.com>
2023-08-18 00:26:46 +0600

    Add files via upload

--- assignment_3.dart
+++ assignment_3.dart
@@ -48,11 +48,11 @@ void main() {
   car3.drive(15000);
 
   print(
-      "Car 1: ${car1.getBrand()} ${car1.getModel()} ${car1.getYear()} Miles: ${car1.getMilesDriven()} Age: ${car1.getAge()}");
+      "Car 1: ${car1.getBrand()} ${car1.getModel()} ${car1.getYear()} Miles: ${car1.getMilesDriven().toInt()} Age: ${car1.getAge()}");
   print(
-      "Car 2: ${car2.getBrand()} ${car2.getModel()} ${car2.getYear()} Miles: ${car2.getMilesDriven()} Age: ${car2.getAge()}");
+      "Car 2: ${car2.getBrand()} ${car2.getModel()} ${car2.getYear()} Miles: ${car2.getMilesDriven().toInt()} Age: ${car2.getAge()}");
   print(
-      "Car 3: ${car3.getBrand()} ${car3.getModel()} ${car3.getYear()} Miles: ${car3.getMilesDriven()} Age: ${car3.getAge()}");
+      "Car 3: ${car3.getBrand()} ${car3.getModel()} ${car3.getYear()} Miles: ${car3.getMilesDriven().toInt()} Age: ${car3.getAge()}");
 
   print("Total number of cars created: ${Car.numberOfCars}");
 }
