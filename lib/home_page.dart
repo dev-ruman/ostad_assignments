43c642f62eff71acb4b0f0aa30174e52ec5700a3
Dev Ruman <sayfurrahman324@gmail.com>
2023-09-08 23:25:00 +0600

    module-five live test design updated

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,77 +7,38 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 100,
-        elevation: 70,
-        shadowColor: Colors.black,
-        backgroundColor: Colors.green,
-        foregroundColor: Colors.white,
-        title: const Text('Home'),
-        leading: const Icon(Icons.add_business),
-        actions: const [
-          Icon(Icons.search),
-        ],
+        title: const Text('Profile'),
         centerTitle: true,
+        elevation: 3,
+        shadowColor: Colors.black,
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
       ),
-      body: const Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'This is mod 5 Assignment',
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.account_circle_rounded,
+              color: Colors.green,
+              size: 48,
             ),
-          ),
-          SizedBox(
-            height: 8,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Text(
-                'My',
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.red,
-                ),
+            Text(
+              'Ruman',
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.green,
               ),
-              SizedBox(
-                width: 6,
+            ),
+            Text(
+              'Flutter Batch 4',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.blue,
               ),
-              Text(
-                'phone',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.blue,
-                ),
-              ),
-              SizedBox(
-                width: 6,
-              ),
-              Text(
-                'name',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.purple,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(
-                width: 6,
-              ),
-              Text(
-                'is Oppo A92',
-                style: TextStyle(
-                  fontSize: 26,
-                  color: Colors.orange,
-                ),
-              ),
-            ],
-          )
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
