6352018f199c5446e33be149101835cae3af4a51
Dev Ruman <sayfurrahman324@gmail.com>
2023-09-24 23:40:46 +0600

    live test code added

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,334 +1,137 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+int initialCount = 0;
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       appBar: AppBar(
-        title: Text('Photo Gallery'),
+        title: const Text('Counter App'),
         centerTitle: true,
         backgroundColor: Colors.grey[900],
         foregroundColor: Colors.white,
       ),
       body: SafeArea(
-        child: SingleChildScrollView(
+        child: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'A Warm Welcome !',
-                  style: TextStyle(
-                    color: Colors.grey[900],
-                    fontWeight: FontWeight.bold,
-                    fontSize: 28,
-                    fontFamily: 'Poppins',
-                  ),
+              const Text(
+                'Count:',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                '$initialCount',
+                style: const TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                      child: TextField(
-                        decoration: InputDecoration(
-                            hintText: 'Search for photos..',
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(12.0))),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        initialCount++;
+                        initialCount >= 5
+                            ? showDialog(
+                                context: context,
+                                builder: (BuildContext context) => Dialog(
+                                      child: Container(
+                                        height: 250,
+                                        padding: const EdgeInsets.symmetric(
+                                          horizontal: 14.0,
+                                          vertical: 12.0,
+                                        ),
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            Text(
+                                              'Button pressed ${initialCount} times.',
+                                              style: TextStyle(
+                                                color: Colors.grey[900],
+                                                fontSize: 22,
+                                              ),
+                                            ),
+                                            const SizedBox(
+                                              height: 40,
+                                            ),
+                                            Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.end,
+                                              children: [
+                                                TextButton(
+                                                  onPressed: () {
+                                                    Navigator.pop(context);
+                                                  },
+                                                  child: Container(
+                                                    padding: const EdgeInsets
+                                                        .symmetric(
+                                                      horizontal: 10.0,
+                                                      vertical: 6.0,
+                                                    ),
+                                                    decoration: BoxDecoration(
+                                                        color: Colors.grey[900],
+                                                        borderRadius:
+                                                            BorderRadius
+                                                                .circular(
+                                                          12.0,
+                                                        )),
+                                                    child: const Text(
+                                                      'CLOSE',
+                                                      style: TextStyle(
+                                                        color: Colors.white,
+                                                        fontSize: 22,
+                                                      ),
+                                                    ),
+                                                  ),
+                                                ),
+                                              ],
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                    ))
+                            : null;
+                      });
+                    },
+                    child: Text(
+                      '+',
+                      style: TextStyle(
+                        color: Colors.grey[900],
+                        fontSize: 32,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        initialCount > 0 ? initialCount-- : null;
+                      });
+                    },
+                    child: Text(
+                      '-',
+                      style: TextStyle(
+                        color: Colors.grey[900],
+                        fontSize: 32,
                       ),
                     ),
                   ),
                 ],
-              ),
-              SizedBox(
-                height: 8.0,
-              ),
-              Container(
-                padding: EdgeInsets.all(12.0),
-                height: 350,
-                child: GridView(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                    childAspectRatio: 4 / 6,
-                    mainAxisSpacing: 10.0,
-                    crossAxisSpacing: 10.0,
-                  ),
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12.0),
-                        color: Colors.white,
-                      ),
-                      child: Column(
-                        children: [
-                          Container(
-                            child: GestureDetector(
-                              onTap: () {
-                                ScaffoldMessenger.of(context)
-                                    .showSnackBar(SnackBar(
-                                  content: Text('Photo Clicked'),
-                                  behavior: SnackBarBehavior.floating,
-                                ));
-                              },
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(12.0),
-                                child: Image.network(
-                                  'https://images.pexels.com/photos/2361952/pexels-photo-2361952.jpeg',
-                                ),
-                              ),
-                            ),
-                          ),
-                          Text(
-                            'Autumn cat',
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12.0),
-                        color: Colors.white,
-                      ),
-                      child: Column(
-                        children: [
-                          Container(
-                            child: GestureDetector(
-                              onTap: () {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(
-                                    content: Text('Photo Clicked'),
-                                    behavior: SnackBarBehavior.floating,
-                                  ),
-                                );
-                              },
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(12.0),
-                                child: Image.network(
-                                  'https://images.pexels.com/photos/2173872/pexels-photo-2173872.jpeg',
-                                ),
-                              ),
-                            ),
-                          ),
-                          Text(
-                            'Brown cat',
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12.0),
-                        color: Colors.white,
-                      ),
-                      child: Column(
-                        children: [
-                          Container(
-                            child: GestureDetector(
-                              onTap: () {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(
-                                    content: Text('Photo Clicked'),
-                                    behavior: SnackBarBehavior.floating,
-                                  ),
-                                );
-                              },
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(12.0),
-                                child: Image.network(
-                                  'https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg',
-                                ),
-                              ),
-                            ),
-                          ),
-                          Text(
-                            'Baby cat',
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12.0),
-                        color: Colors.white,
-                      ),
-                      child: Column(
-                        children: [
-                          Container(
-                            child: GestureDetector(
-                              onTap: () {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(
-                                    content: Text('Photo Clicked'),
-                                    behavior: SnackBarBehavior.floating,
-                                  ),
-                                );
-                              },
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(12.0),
-                                child: Image.network(
-                                  'https://images.pexels.com/photos/121920/pexels-photo-121920.jpeg',
-                                ),
-                              ),
-                            ),
-                          ),
-                          Text(
-                            'Sleepy cat',
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12.0),
-                        color: Colors.white,
-                      ),
-                      child: Column(
-                        children: [
-                          Container(
-                            child: GestureDetector(
-                              onTap: () {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(
-                                    content: Text('Photo Clicked'),
-                                    behavior: SnackBarBehavior.floating,
-                                  ),
-                                );
-                              },
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(12.0),
-                                child: Image.network(
-                                  'https://images.pexels.com/photos/1317844/pexels-photo-1317844.jpeg',
-                                ),
-                              ),
-                            ),
-                          ),
-                          Text(
-                            'Sneaky cat',
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12.0),
-                        color: Colors.white,
-                      ),
-                      child: Column(
-                        children: [
-                          Container(
-                            child: GestureDetector(
-                              onTap: () {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(
-                                    content: Text('Photo Clicked'),
-                                    behavior: SnackBarBehavior.floating,
-                                  ),
-                                );
-                              },
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(12.0),
-                                child: Image.network(
-                                  'https://images.pexels.com/photos/2524164/pexels-photo-2524164.jpeg',
-                                ),
-                              ),
-                            ),
-                          ),
-                          Text(
-                            'Dark cat',
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Container(
-                height: 210,
-                child: ListView(
-                  children: [
-                    ListTile(
-                      title: Text('Pran Milk'),
-                      leading: Image.asset('assets/images/uht.webp'),
-                      subtitle: Text('Pran Full cream pasturized milk'),
-                    ),
-                    ListTile(
-                      title: Text('Uht Milk'),
-                      leading: Image.asset('assets/images/pran.webp'),
-                      subtitle: Text('Pran UHT milk'),
-                    ),
-                    ListTile(
-                      title: Text('Diploma'),
-                      leading: Image.asset('assets/images/diploma.webp'),
-                      subtitle: Text('Diploma milk powder'),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 14.0,
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 12.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                        child: GestureDetector(
-                      onTap: () {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: Text('Photos Uploaded Successfully!'),
-                            behavior: SnackBarBehavior.floating,
-                          ),
-                        );
-                      },
-                      child: Container(
-                        padding: EdgeInsets.all(12.0),
-                        decoration: BoxDecoration(
-                          color: Colors.grey[900],
-                          borderRadius: BorderRadius.circular(12.0),
-                        ),
-                        child: Icon(
-                          Icons.upload_rounded,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ))
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 40,
               ),
             ],
           ),
